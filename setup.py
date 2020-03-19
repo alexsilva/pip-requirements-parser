cafe060d61acb538284d7ce0c740a30eb2589d6d
alex <geniofuturo@gmail.com>
2020-03-19 10:53:41 -0300

    v2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pip-requirements-parser',
-    version='1.0.0',
+    version='2.0.0',
     url='https://github.com/alexsilva/pip-requirements-parser',
     license='MIT',
     author='alex',
